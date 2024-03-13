1a4c719276152fe02e27333822ff670617996c29
Amit Aryan <aryanamit122@gmail.com>
2024-03-13 21:13:36 +0530

    added

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
-let btn1=document.querySelector(".searchbar");
+window.addEventListener('scroll', function() {
+    var header = document.querySelector('.header');
+    var main = document.querySelector('.main');
 
-btn1.addEventListener("click"),() =>{
-    
-}
-
+    if (window.scrollY > 0) {
+        header.style.boxShadow = '5px 5px 30px rgba(0, 0, 0, 0.5)';
+        main.style.paddingTop = header.offsetHeight + 'px';
+    } else {
+        header.style.boxShadow = 'none';
+        main.style.paddingTop = '';
+    }
+});
